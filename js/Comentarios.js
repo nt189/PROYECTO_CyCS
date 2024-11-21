192c117ad7d2dd4a04ca45aaa2e6a429d12cc320
nt189 <133317916+nt189@users.noreply.github.com>
2024-11-21 04:17:12 +0000

    Carga de comentarios

--- js/Comentarios.js
+++ js/Comentarios.js
@@ -1,4 +1,4 @@
-function comentarios(){
+function seccioncomentarios(){
     if (sessionStorage.getItem('id')){
         var div = document.createElement('div');
         var comentario = `
@@ -15,14 +15,63 @@ function comentarios(){
         document.getElementById('mainComentarios').appendChild(div);
     }
 }
-comentarios();
+seccioncomentarios();
+
+function comentarios(){
+    var comentarios = JSON.parse(localStorage.getItem('comentarios'));
+    var usr = JSON.parse(localStorage.getItem('Usuarios'))
+    
+    for (let id = 0; id < comentarios.length; id++) {
+        for (let com = 0; com < comentarios[id].length; com++) {
+            var template = `
+                <div>
+                    <p><strong>${usr[id][0]}</strong>:${comentarios[id][com]}</p>
+                </div>
+            `
+
+            var div = document.createElement('div');
+            div.innerHTML = template;
+
+            var reviewsContainer = document.getElementsByClassName('user-reviews')[0]; // Obtiene el primer elemento con la clase 'user-reviews'
+            reviewsContainer.appendChild(div);
+        }
+    }
+}
+comentarios()
 
 function cargarcomentario(){
+    var id = sessionStorage.getItem('id');
     var comentario = document.getElementById('comentario').value;
+    var usr = JSON.parse(localStorage.getItem('Usuarios'))
 
-    Restaurante = {
-        "id": sessionStorage.getItem('id'),
-        "comentario": comentario,
-    };
+    if(localStorage.getItem('comentarios')){
+        var comentarios = JSON.parse(localStorage.getItem('comentarios'));
 
+        if (!comentarios[id]) {
+            comentarios[id] = []; 
+        }
+
+        comentarios[id].push(comentario)
+        localStorage.removeItem('comentarios')
+        localStorage.setItem('comentarios',JSON.stringify(comentarios))
+        console.log(comentarios)
+    }
+    else{
+        var comaux = Array();
+        comaux[id] = [];
+        comaux[id].push(comentario)
+        localStorage.setItem('comentarios',JSON.stringify(comaux))
+    }
+
+    var template = `
+                <div>
+                    <p><strong>${usr[id][0]}</strong>:${comentarios[id][comentarios[id].length-1]}</p>
+                </div>
+            `
+
+    var div = document.createElement('div');
+    div.innerHTML = template;
+
+    var reviewsContainer = document.getElementsByClassName('user-reviews')[0]; // Obtiene el primer elemento con la clase 'user-reviews'
+    reviewsContainer.appendChild(div);
 }
